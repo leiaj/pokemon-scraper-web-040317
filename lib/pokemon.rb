5008b96b3361599b3fb14677782773acd6e52e73
github username <github email address>
2017-04-11 15:26:25 -0400

    Completed with bonusgit add .git add .

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,18 +1,21 @@
 require 'pry'
 class Pokemon
 
-  attr_accessor :name, :type
+  attr_accessor :name, :type, :hp
   attr_reader :db, :id
 
-  # def initialize(name:, id:, type:, db:)
-  #   @name = name
-  #   @id = id
-  #   @type = type
-  #   @db = db
-  # end
-  def initialize(arguments)
-    arguments.each {|k,v| instance_variable_set("@#{k}",v)}
+  def initialize(name:, id:, type:, db:, hp: nil)
+    @name = name
+    @id = id
+    @type = type
+    @db = db
+    @hp = hp
+
   end
+
+  # def initialize(arguments)
+  #   arguments.each {|k,v| instance_variable_set("@#{k}",v)}
+  # end
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
@@ -22,8 +25,13 @@ class Pokemon
     new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     #new_pokemon = [[1,"Pikachu", "electric"]]
     # binding.pry
-    Pokemon.new(id: new_pokemon[0][0], name: new_pokemon[0][1], type: new_pokemon[0][2])
+    Pokemon.new(hp: new_pokemon[0][3], id: new_pokemon[0][0], name: new_pokemon[0][1], type: new_pokemon[0][2], db: db)
     # binding.pry
+  end
+
+  def alter_hp(hp, db)
+    @hp = hp
+    db.execute("UPDATE pokemon SET hp = ?", hp)
   end
 
 
